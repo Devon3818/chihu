1e410e3852fbf4dc50f994ebc34a94b82d3db2b6
Devon <k849996781@vip.qq.com>
2017-04-01 12:47:16 +0800

    4-1

--- src/pages/discover/discover.ts
+++ src/pages/discover/discover.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { AnswerPage } from '../answer/answer';
+import { QuestionPage } from '../question/question';
+import { ArticlePage } from '../article/article';
 
 /*
   Generated class for the Discover page.
@@ -15,18 +18,41 @@ declare var Swiper: any;
 })
 export class DiscoverPage {
 
-  oSwiper:any = null;
+  oSwiper1:any = null;
+  oSwiper2:any = null;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
-    this.oSwiper = new Swiper('.swiper-container', {
+    this.oSwiper1 = new Swiper('.swiper-container', {
         loop: true,
         autoplay: 5000,
         autoplayDisableOnInteraction: false,
         // 如果需要分页器
         pagination: '.swiper-pagination',
       });
+
+      this.oSwiper2 = new Swiper('.swiper-container2', {
+        slidesPerView: 3,
+        paginationClickable: true,
+        spaceBetween: 7,
+        freeMode: true
+      });
   }
+
+  //打开页面
+    pushAnswerPage(){
+      this.navCtrl.push( AnswerPage );
+    }
+
+    //打开页面
+    pushQuestionPage(){
+      this.navCtrl.push( QuestionPage );
+    }
+
+    //打开页面
+    pushArticlePage(){
+      this.navCtrl.push( ArticlePage );
+    }
 
 }
